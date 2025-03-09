3dafb0953cb686414ee76d1cd2ef85a2033dbb0d
AnnaEscrisa <anna.escrisa@gmail.com>
2025-03-09 16:43:37 +0100

    hotfix bug1

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+//comentari hotfix1
 <!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
 <!--[if IE 7]> <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
 <!--[if IE 8]> <html class="lt-ie9" lang="en"> <![endif]-->
